42cf512c52f97583cf780f879ed8044ee0fdaf53
SaurHub007 <161135159+SaurHub007@users.noreply.github.com>
2025-06-20 11:32:48 +0530

    Add files via upload

---
+++ Python/if_elif_else.py
@@ -0,0 +1,61 @@
+'''Profit > 0
+Profit < 0
+Profit <= 0
+Profit >= 0
+Profit != 0 '''
+#---------------------------------------------
+#Understanding If
+''' Profit = int(input('Enter The Profit : '))
+if Profit <0:
+    print('Alert ')
+    print ('Everyone !!!' ,end='')
+    print('Profit is in Negative...!')
+print('See You Again')  '''  
+
+#--------------------------------------------
+
+#understanding If else
+'''Profit = int(input('Enter The Profit : '))
+if Profit <0:
+    print('Alert ')
+    print ('Everyone !!!' ,end='')
+    print('Profit is in Negative...!')
+else :
+    print ('WOwww')
+    print ('Everyone !!!')
+    print('We are in Profitable')
+    print('Now Lets do Fucking Party')
+print('See You Again')'''
+
+#--------------------------------------------
+'''Profit = int(input('Enter The Profit : '))
+if Profit <50:
+    print('Alert ')
+    print ('Everyone !!!' ,end='')
+    print('Profit is in Negative...!')
+elif Profit == 0:
+    print('We are Not in Negative')
+    print('But not also Profitable')
+    print('So Do Hardwork Othervise You gonna Fired')
+else :
+    print ('WOwww')
+    print ('Everyone !!!')
+    print('We are in Profitable')
+    print('Now Lets do Fucking Party')
+print('See You Again')'''
+
+#--------------------------------------------
+n=int(input('Enter Number : '))
+if n>0:
+    print('Its Postive')
+elif n==0 :
+    print('Its Zero')
+else :
+    print('Its negative')
+print('see you again')
+
+
+
+
+
+
