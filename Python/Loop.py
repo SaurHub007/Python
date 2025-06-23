715fbef706d6b2deda7b9d56421f638e2b81861f
SaurHub007 <161135159+SaurHub007@users.noreply.github.com>
2025-06-23 11:49:54 +0530

    Add files via upload

---
+++ Python/Loop.py
@@ -0,0 +1,68 @@
+'''n = int(input('Enter The Number'))
+if n%2==0:
+    print('Even Number')
+else:
+    print('Odd Number')'''
+#------------------------------------------
+'''a= int(input('Enter frist The Number'))
+b= int(input('Enter second The Number'))
+
+if a == 0:
+    print('frist number is Zero')
+elif a<b:
+    print('Second Number is Big')
+    
+elif a>b:
+    print('First Number is Big')
+else :
+    print('Both are Same')'''
+#------------------------------------------
+'''a= int(input('Enter The Number'))
+
+if a >=75 and a<=100:
+    print('A')
+elif a >=60 and a<=74:
+    print('b')
+elif a >=35 and a<=59:
+    print('c')
+elif a >=0 and a<=34:
+    print('f')
+else :
+    print('thik se number dal tu 0-100 main')'''
+#------------------------------------------
+'''x= input('Enter The aphabate :')
+#if x== 'a' or x== 'e' or x == 'i' or x == 'o' or x == 'u' :
+if x in ('a','e','o','i','o','u','A','E','I','O','U'):
+     print ('vowel')
+else :
+     print('consonant')'''
+#---------------for Loop---------------------------
+'''for i in range (5,26):
+    print(i)'''
+#------------------------------------------
+'''for i in ('a','e','i','o','u'):
+    print(i)'''
+#------------------------------------------
+'''for i in range(1,21):
+    print(f'2*{i} = {2*i}')'''
+#------------------------------------------
+'''for i in range(1,7):
+    print(i)
+'''
+#---------------While Loop---------------------------
+'''j=1
+while j<=6:
+ print(j)
+ j = j+1  '''
+
+
+
+
+
+
+
+
+
+
+
+
